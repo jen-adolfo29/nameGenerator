af9ba137b8296b50f3da759d7776a8da30fcc5cb
jen-adolfo29 <jenadolfo5@gmail.com>
2023-08-20 08:37:07 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# pet name generator
+import random
+names = ["bella", "aly", "erin", "chamcham", "cams",
+             "coco", "toby", "lola", "jack", "pete",
+             "manny", "milo", "koala", "momo", "brook",
+             "jinbei", "doffy", "oven", "pudding", "cobra",
+         "jenna", "edison", "luke", "diana", "paolo"]
+generated = random.choice(names)
+print("Name generated: ", generated)
